fffe965ad0136268ee2181553c7c3e476294d501
lucaducceschi <lucaducceschi@gmail.com>
2022-03-10 18:56:08 +0100

    modded service

--- nlp4all_web_app/pipeline_service.py
+++ nlp4all_web_app/pipeline_service.py
@@ -53,27 +53,6 @@ def getfilter():
     language = request_data['language']
     return language
 
-@app.route('/json_example', methods=['POST'])
-def json_example():
-    request_data = request.get_json()
-
-    language = request_data['language']
-    framework = request_data['framework']
-
-    # two keys are needed because of the nested object
-    python_version = request_data['version_info']['python']
-
-    # an index is needed because of the array
-    example = request_data['examples'][0]
-
-    boolean_test = request_data['boolean_test']
-
-    return '''
-           The language value is: {}
-           The framework value is: {}
-           The Python version is: {}
-           The item at index 0 in the example list is: {}
-           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
 if __name__ == "__main__":
     app.run(debug=True)
 
